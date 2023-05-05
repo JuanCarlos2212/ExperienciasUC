1b3dffbc075adcd3531f4bb4a958d8ea694e3059
pomalima <pomalima.22@gmail.com>
2023-05-05 18:52:31 -0500

    menu de navegación

--- app/src/main/java/com/example/experienciasuc/MainActivity.java
+++ app/src/main/java/com/example/experienciasuc/MainActivity.java
@@ -29,19 +29,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         setSupportActionBar(binding.appBarMain.toolbar);
-        binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        //binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
+            //@Override
+            //public void onClick(View view) {
+                //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        //.setAction("Action", null).show();
+           // }
+        //});
         DrawerLayout drawer = binding.drawerLayout;
         NavigationView navigationView = binding.navView;
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.nav_plan_estudios)
+                R.id.nav_inicio, R.id.nav_plan, R.id.nav_informacion, R.id.nav_eventos, R.id.nav_contactanos)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
