2f8c791d7cab7e8b0019ba8973ff4106b4da8888
NathG2704 <nathaly.gutierrez.2704@gmail.com>
2023-05-11 12:53:46 -0500

    cambios en el layout de lista ciclos

--- app/src/main/java/UtilExperiencias/Utilidades.java
+++ app/src/main/java/UtilExperiencias/Utilidades.java
@@ -4,4 +4,5 @@ public class Utilidades {
 
     public static final String RUTA= "https://restapi-production-575c.up.railway.app/api/";
 //    public static final String RUTA= "http://192.168.100.44/ExperienciasUC/";
+//    public static final String RUTA= "http://192.168.100.44/ExperienciasUC/";
 }
